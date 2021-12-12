e98193f72684561c226752d6869d8e98c542084e
Denis Nazarovs <denis.nazarovs@va.lv>
2021-12-12 15:06:09 +0200

    Denis' appliaction

--- App.js
+++ App.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { FlatList, StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "React application" }}
+        />
+        <Stack.Screen name="List View"
+        component={FlatListBasics} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeScreen = ({ navigation }) => {
+  const [counter, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.button}>
+        <Button
+          title="List View"
+          onPress={() => navigation.navigate("List View")}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button title="Increase count" onPress={() => setCount(counter + 1)} />
+      </View>
+      <View style={styles.button}>
+        <Button title="Decrease count" onPress={() => setCount(counter - 1)} />
+      </View>
+      <View>
+        <Text> Current count: {counter}</Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  button: {
+    margin : 15,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+  },
+  item: {
+    padding: 16,
+    fontSize: 40,
+    color : "white",
+  },
+  itemBox: {
+    backgroundColor: "dimgray",
+    padding: 5,
+    borderRadius: 7,
+    margin: 8,
   },
 });
+
+const list = [
+  { name: "John Doe" },
+  { name: "John Doe" },
+  { name: "John Doe" },
+  { name: "John Doe" },
+  { name: "John Doe" },
+];
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={list}
+        renderItem={({ item }) => (
+          <View style={styles.itemBox}>
+            <Text style={styles.item}>{item.name} </Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default MyStack;
