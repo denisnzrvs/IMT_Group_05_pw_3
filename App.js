224e2d297904072e71b65b4df8e198d5a86a40c1
Maryia Brauer <maryia.brauer@va.lv>
2021-12-12 16:34:07 +0300

    Maryia's app

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { FlatList, StyleSheet, Text, View, Button } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { FlatList, Button, StyleSheet, View, Text } from 'react-native';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -12,65 +14,75 @@ const MyStack = () => {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: "React application" }}
+          options={{ title: 'React application' }}
         />
-        <Stack.Screen name="List View"
-        component={FlatListBasics} />
+        <Stack.Screen name="List View" component={FlatListBasics} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 const HomeScreen = ({ navigation }) => {
-  const [counter, setCount] = useState(0);
+const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <View style={styles.button}>
+       <View style={styles.buttonContainer}>
         <Button
-          title="List View"
-          onPress={() => navigation.navigate("List View")}
-        />
-      </View>
-      <View style={styles.button}>
-        <Button title="Increase count" onPress={() => setCount(counter + 1)} />
-      </View>
-      <View style={styles.button}>
-        <Button title="Decrease count" onPress={() => setCount(counter - 1)} />
-      </View>
-      <View>
-        <Text> Current count: {counter}</Text>
-      </View>
+          title="Go to List View"
+          onPress={() =>
+          navigation.navigate('List View')
+      }
+    />
     </View>
+       <View style={styles.buttonContainer}>
+       	<Button
+        title="Increase count"
+        onPress={() => setCount(count + 1)}
+        />
+        </View>
+        <View style={styles.buttonContainer}>
+         <Button
+         title="Decrease count"
+         onPress={() => setCount(count - 1)}
+         />
+         </View>
+         <View>
+	        <Text> Current count: {count}</Text>
+	         <Text style={styles.decor}></Text>
+	       </View>
+         </View>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
-    margin : 15,
-  },
+  buttonContainer: {
+   margin: 20,
+ },
   container: {
     flex: 1,
-    paddingTop: 40,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  item: {
-    padding: 16,
-    fontSize: 40,
-    color : "white",
+ item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
-  itemBox: {
-    backgroundColor: "dimgray",
-    padding: 5,
-    borderRadius: 7,
-    margin: 8,
-  },
+  itemContainer: {
+  backgroundColor: "grey",
+  padding: 5,
+  borderRadius: 4,
+  margin: 8,
+},
 });
 
 const list = [
-  { name: "John Doe" },
-  { name: "John Doe" },
-  { name: "John Doe" },
-  { name: "John Doe" },
-  { name: "John Doe" },
+  { name: "John Doe 1" },
+  { name: "John Doe 2" },
+  { name: "John Doe 3" },
+  { name: "John Doe 4" },
+  { name: "John Doe 5" },
 ];
 
 const FlatListBasics = () => {
@@ -78,11 +90,11 @@ const FlatListBasics = () => {
     <View style={styles.container}>
       <FlatList
         data={list}
-        renderItem={({ item }) => (
-          <View style={styles.itemBox}>
-            <Text style={styles.item}>{item.name} </Text>
-          </View>
-        )}
+        renderItem={({item}) => (
+         <View style={styles.itemContainer}>
+        <Text style={styles.item}>{item.name}</Text>
+        </View>
+      )}
       />
     </View>
   );
